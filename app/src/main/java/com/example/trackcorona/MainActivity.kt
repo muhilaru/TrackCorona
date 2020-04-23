1d9d6a4a1b399465facbaacadb699c7b49f461f5
muhila <muhila2@illinois.edu>
2020-04-22 23:54:50 -0500

    Set up scrolling background for main menu with title text and start button

--- app/src/main/java/com/example/trackcorona/MainActivity.kt
+++ app/src/main/java/com/example/trackcorona/MainActivity.kt
@@ -8,23 +8,39 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import com.example.trackcorona.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
+        setContentView(R.layout.main_menu)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        var mainButton = findViewById<Button>(R.id.mmbutton)
+        mainButton.visibility = View.VISIBLE
+
+        var mainTitle = findViewById<TextView>(R.id.maintitle)
+        mainTitle.visibility = View.VISIBLE
+
+
+//        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+//        val viewPager: ViewPager = findViewById(R.id.view_pager)
+//        viewPager.adapter = sectionsPagerAdapter
+//        val tabs: TabLayout = findViewById(R.id.tabs)
+//        tabs.setupWithViewPager(viewPager)
+//        val fab: FloatingActionButton = findViewById(R.id.fab)
+//
+//        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
+    }
+
+     fun loadHeatMap() {
+//         var mProvider = new HeatmapTileProvider.Builder();
+
     }
 }
