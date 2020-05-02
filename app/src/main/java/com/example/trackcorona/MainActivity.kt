2a1735a06183649665ee8a35ba4526047a0c6eca
muhila <muhila2@illinois.edu>
2020-05-01 21:10:02 -0500

    Refactored variables to follow google style guidelines, added constants to replace magic numbers

--- app/src/main/java/com/example/trackcorona/MainActivity.kt
+++ app/src/main/java/com/example/trackcorona/MainActivity.kt
@@ -2,61 +2,36 @@ package com.example.trackcorona
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import com.example.trackcorona.ui.main.SectionsPagerAdapter
-import java.io.File
-import java.io.FileOutputStream
-import java.net.URL
 import android.os.StrictMode
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
-import com.google.android.gms.maps.model.LatLng
-import com.jjoe64.graphview.helper.GraphViewXML
 
 
 class MainActivity : AppCompatActivity() {
-
-    private var piechart_map = HashMap<Int, String>()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_menu)
 
-        //temporary solution to reading online dataset
-        //TODO: Implement Async task to avoid running network connection on main thread
+        // temporary solution to reading online data set
+        // TODO: Implement Async task to avoid running network connection on main thread
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
 
 
-
-
+        // setting up start button and title for home screen
         val mainButton = findViewById<Button>(R.id.mmbutton)
         mainButton.visibility = View.VISIBLE
 
         val mainTitle = findViewById<TextView>(R.id.maintitle)
         mainTitle.visibility = View.VISIBLE
 
+        // launching google map visualization
         mainButton.setOnClickListener {
             val loadMap = Intent(this, VirusMap::class.java)
             startActivity(loadMap)
         }
     }
-
-
-
 }
--- app/src/main/java/com/example/trackcorona/VirusMap.kt
+++ app/src/main/java/com/example/trackcorona/VirusMap.kt
@@ -1,6 +1,5 @@
 package com.example.trackcorona
 
-import android.app.Activity
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,95 +13,105 @@ import com.google.maps.android.clustering.algo.NonHierarchicalDistanceBasedAlgor
 import com.google.maps.android.heatmaps.HeatmapTileProvider
 import com.google.maps.android.heatmaps.WeightedLatLng
 import java.net.URL
-import java.nio.charset.Charset
-import java.io.*
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import android.provider.CalendarContract
-import android.view.View.inflate
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import kotlin.text.Charsets.UTF_8
-import com.example.trackcorona.MainActivity
 import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.utils.ColorTemplate
 
 
 class VirusMap : AppCompatActivity(), OnMapReadyCallback {
 
+    // google map
     private lateinit var mMap: GoogleMap
-    private var most_cases_us: Double = 0.0
-    private var piechart_names = ArrayList<String>()
-    private var piechart_values = ArrayList<Int>()
-    private var piechart_map = HashMap<Int, String>()
+    // Hash map to pair countries with confirmed case count
+    private var pieChartMap = HashMap<Int, String>()
+    private var mostCasesUSA: Double = 0.0
+
+    // using COVID-19 data from Johns Hopkins University
+    //https://github.com/CSSEGISandData/COVID-19
+    private val usaData =
+        "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+    private val globalData =
+        "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+
+    private val PROVINCE = 0
+    private val COUNTRY = 1
+    private val LAT = 2
+    private val LNG = 3
 
 
+    private val US_LAT = 8
+    private val US_LNG = 9
+    private val COUNTY = 5
+    private val STATE = 6
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_virus_map)
+        setContentView(R.layout.virus_map)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        println("WORKING")
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+    // this callback is triggered when the map is ready to be used
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        // list of weighted lat lng coordinates used to create heat map
         var listOfDataPoints = mutableListOf<WeightedLatLng>()
 
+        // market cluster manager to add markers on map
         var mClusterManager = ClusterManager<DataPoint>(this, mMap)
+        // setting algorithm to showcase market distribution most accurately
         mClusterManager.setAlgorithm(NonHierarchicalDistanceBasedAlgorithm<DataPoint>())
 
         mMap.setOnCameraIdleListener(mClusterManager)
         mMap.setOnMarkerClickListener(mClusterManager)
 
-
-        var url =
-            "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
-        var reader = URL(url).openStream().bufferedReader(UTF_8)
-
+        // iterating line by line of data set csv file
+        var reader = URL(usaData).openStream().bufferedReader(UTF_8)
         var line: String? = ""
+
+
         while (line != null) {
+
             line = reader.readLine()
+
             if (line == null) {
                 break
             }
+
             val row = line.split(",")
 
-            if (row.size > 1 && row[0] != "UID" && row[9] != "" && row[8].toDouble() != 0.0) {
-                val coordinates = LatLng(row[8].toDouble(), row[9].toDouble())
-                var name = row[6]
 
-                if (row[5].isNotBlank()) {
-                    name += " (" + row[5] + " County)"
+            //skipping first row of column labels
+            //skipping cruise ships (labeled with lat lng of (0.0, 0.0)
+            //ensuring latitue and longitude are provided
+            if (row[0] != "UID" && row[US_LNG] != "" && row[US_LAT].toDouble() != 0.0) {
+
+
+                val coordinates = LatLng(row[US_LAT].toDouble(), row[US_LNG].toDouble())
+                var name = row[STATE]
+
+                if (row[COUNTY].isNotBlank()) {
+                    name += " (" + row[COUNTY] + " County)"
                 }
 
-                val num_of_cases = row[row.size - 1].toDouble()
+                val numOfCases = row[row.size - 1].toDouble()
 
-                if (most_cases_us < num_of_cases) {
-                    most_cases_us = num_of_cases
+                if (mostCasesUSA < numOfCases) {
+                    mostCasesUSA = numOfCases
                 }
 
 
-                val locationData = DataPoint(name, coordinates, row[row.size - 1].toDouble())
+                val locationData = DataPoint(name, coordinates, numOfCases)
 
                 mClusterManager.addItem(locationData)
                 listOfDataPoints.add(locationData.makeWeighted())
@@ -110,9 +119,8 @@ class VirusMap : AppCompatActivity(), OnMapReadyCallback {
         }
 
 
-        url =
-            "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-        reader = URL(url).openStream().bufferedReader(UTF_8)
+
+        reader = URL(globalData).openStream().bufferedReader(UTF_8)
 
         line = ""
         while (line != null) {
@@ -120,30 +128,38 @@ class VirusMap : AppCompatActivity(), OnMapReadyCallback {
             if (line == null) {
                 break
             }
+
             val row = line.split(",")
 
-            if (row[1] == "\"Korea") {
+            if (row[COUNTRY] == "\"Korea") {
                 val coordinates = LatLng(row[3].toDouble(), row[4].toDouble())
-                var name = "South Korea"
-                val locationData = DataPoint(name, coordinates, row[row.size - 1].toDouble())
+                val name = "South Korea"
+                val numOfCases = row[row.size - 1].toDouble()
+                val locationData = DataPoint(name, coordinates, numOfCases)
                 mClusterManager.addItem(locationData)
-                piechart_map.put(row[row.size - 1].toInt(), name)
-            } else if (row[0] == "\"Bonaire") {
+                pieChartMap.put(numOfCases.toInt(), name)
+
+
+            } else if (row[PROVINCE] == "\"Bonaire") {
                 val coordinates = LatLng(row[3].toDouble(), row[4].toDouble())
                 var name = "Netherlands (Bonaire, Sint Eustatius, and Saba"
                 val locationData = DataPoint(name, coordinates, row[row.size - 1].toDouble())
                 mClusterManager.addItem(locationData)
-                piechart_map.put(row[row.size - 1].toInt(), name)
+                pieChartMap.put(row[row.size - 1].toInt(), name)
+
+
             } else {
-                if (row.size > 1 && row[2] != "Lat" && row[2].toDouble() != 0.0) {
-                    val coordinates = LatLng(row[2].toDouble(), row[3].toDouble())
-                    var name = row[1]
-                    if (row[0].isNotBlank()) {
-                        name += " (" + row[0] + ")"
+                if (row.size > 1 && row[LAT] != "Lat" && row[LAT].toDouble() != 0.0) {
+
+
+                    val coordinates = LatLng(row[LAT].toDouble(), row[LNG].toDouble())
+                    var name = row[COUNTRY]
+                    if (row[PROVINCE].isNotBlank()) {
+                        name += " (" + row[PROVINCE] + ")"
                     }
                     val locationData = DataPoint(name, coordinates, row[row.size - 1].toDouble())
                     mClusterManager.addItem(locationData)
-                    piechart_map.put(row[row.size - 1].toInt(), name)
+                    pieChartMap.put(row[row.size - 1].toInt(), name)
 
                 }
             }
@@ -151,13 +167,13 @@ class VirusMap : AppCompatActivity(), OnMapReadyCallback {
 
 
         val mProvider = HeatmapTileProvider.Builder().weightedData(listOfDataPoints).build()
-        mProvider.setMaxIntensity(most_cases_us / 2.0)
+        mProvider.setMaxIntensity(mostCasesUSA / 2.0)
         mProvider.setRadius(50)
         val mOverlay = mMap.addTileOverlay(TileOverlayOptions().tileProvider(mProvider))
 
 
         val graphButton = findViewById<Button>(R.id.graphview)
-        graphButton.setOnClickListener{
+        graphButton.setOnClickListener {
             setUpChart()
         }
 
@@ -165,13 +181,12 @@ class VirusMap : AppCompatActivity(), OnMapReadyCallback {
 
     fun setUpChart() {
 
-        val sorted = piechart_map.toSortedMap(reverseOrder())
+        val sorted = pieChartMap.toSortedMap(reverseOrder())
         var counter = 0
 
         var pieEntries = ArrayList<PieEntry>()
 
         for ((key, value) in sorted) {
-            //println(value + " " + key)
 
             if (counter == 10) {
                 break
@@ -205,8 +220,6 @@ class VirusMap : AppCompatActivity(), OnMapReadyCallback {
 
 
     }
-
-
 
 
 }
